b2c656392eb7e6ccd5291f6ca8073161a7114213
Nicholas Tengbumroong <cherryappletruck@gmail.com>
2022-07-07 17:23:05 -0700

    Updated sample posts

--- components/Post.js
+++ components/Post.js
@@ -8,7 +8,7 @@ function Post({ post }) {
         className="grid p-4 overflow-hidden md:grid-cols-5 rounded-xl lg:p-6 xl:grid-cols-12 hover:bg-gray-50"
       >
         <h3 className="mb-1 ml-8 font-semibold md:col-start-2 md:col-span-4 md:ml-0 xl:col-start-3 xl:col-span-9">
-          {post.orgID}
+          {`OrgID: ` + post.orgID}
         </h3>
         <time
           dateTime=""
--- pages/index.js
+++ pages/index.js
@@ -32,28 +32,28 @@ export async function getServerSideProps(context) {
       postID: 1,
       orgID: 1,
       date: 'July 7, 2022',
-      msg: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      msg: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum lacinia risus in magna vehicula, nec imperdiet dui pellentesque. Suspendisse fringilla elit id dapibus feugiat. Aliquam imperdiet aliquam eros ut porttitor. Morbi diam risus, vehicula ut purus at, aliquet tempor felis.',
       msgAtch: null
     },
     {
       postID: 2,
       orgID: 1,
-      date: 'July 7, 2022',
-      msg: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      date: 'July 9, 2022',
+      msg: 'Nulla eu orci ac ligula vehicula accumsan vel et ex. Sed feugiat porta lectus, gravida tincidunt justo. Phasellus ac cursus enim. Ut lacus sapien, aliquet sit amet aliquet a, porta aliquam nisi. Nunc vitae dolor et nisl porta fringilla in vel felis. Morbi in orci et augue sodales egestas.',
       msgAtch: null
     },
     {
       postID: 3,
       orgID: 2,
-      date: 'July 7, 2022',
-      msg: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      date: 'July 12, 2022',
+      msg: 'Proin ullamcorper ex ut laoreet maximus. Vestibulum in purus sit amet metus tristique sollicitudin vitae eu lectus. Donec vestibulum est id euismod pretium. Nam lacus nisl, porttitor eu ante et, hendrerit pulvinar velit. Nulla aliquet at leo vitae suscipit. Etiam eget dictum tellus. Nullam tristique magna odio, sed sollicitudin velit blandit quis. Donec lectus tellus, consectetur quis lorem sit amet, imperdiet dictum ex. Pellentesque fermentum id massa ut maximus.',
       msgAtch: null
     },
     {
       postID: 4,
       orgID: 3,
-      date: 'July 7, 2022',
-      msg: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      date: 'August 4, 2022',
+      msg: 'Sed eget laoreet sapien, non dapibus dolor. Maecenas molestie, tortor ac sodales tempus, dui erat consectetur ante, id facilisis eros quam id orci. In aliquam, ligula at mollis porta, arcu felis pretium tellus, tempus finibus leo nibh eu arcu. In sagittis lectus lacus, et lobortis lacus ultrices vel. Pellentesque metus magna, fermentum a magna et, faucibus faucibus mi. Ut semper nulla sed scelerisque vestibulum. Pellentesque a odio turpis. Aliquam et iaculis magna.',
       msgAtch: null
     },
   ]; 
