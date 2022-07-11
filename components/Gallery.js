8255a81ef50433585dba3e8fb3f84b8624d08d66
Nicholas Tengbumroong <cherryappletruck@gmail.com>
2022-07-10 23:14:05 -0700

    Added link to image on click

--- components/Gallery.js
+++ components/Gallery.js
@@ -1,20 +1,26 @@
 import Image from "next/image";
 
 function Gallery({ images }) {
-
   return (
-    <section className="bg-gray-200">
+    <section className="bg-gray-100">
       <div className="container flex flex-col justify-center p-4 mx-auto">
-        <div className="grid grid-cols-1 gap-4 lg:grid-cols-4 sm:grid-cols-2">
+        <div className="grid grid-cols-1 gap-4 lg:grid-cols-5 sm:grid-cols-2">
           {images?.map((image, index) => (
-            <Image
+            <a
               key={index}
-              src={image}
-              alt=""
-              width={128}
-              height={128}
-              className="object-cover w-full bg-gray-500 aspect-square"
-            />
+              href={image}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image
+                src={image}
+                alt=""
+                width={64}
+                height={64}
+                layout="responsive"
+                className="object-cover w-full bg-gray-500 aspect-square"
+              />
+            </a>
           ))}
         </div>
       </div>
