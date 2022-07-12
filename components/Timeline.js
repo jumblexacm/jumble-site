19843b8864fbc96d9645753eed12083d5a9ef945
Nicholas Tengbumroong <cherryappletruck@gmail.com>
2022-07-11 19:47:07 -0700

    Added show more button and limited default num of images shown

--- components/Timeline.js
+++ components/Timeline.js
@@ -1,15 +1,15 @@
 import Post from "./Post";
 
 function Timeline({ posts }) {
-  return (
-    <ul className="p-4 lg:p-8 bg-gray-400 text-gray-800 space-y-3">
-      {posts?.map((post, index) => (
-        <li key={index}>
-          <Post post={post}/>
-        </li>
-      ))}
-    </ul>
-  );
+	return (
+		<ul className='p-4 lg:p-8 bg-gray-200 text-gray-800 space-y-3'>
+			{posts?.map((post, index) => (
+				<li key={index}>
+					<Post post={post} />
+				</li>
+			))}
+		</ul>
+	);
 }
 
 export default Timeline;
