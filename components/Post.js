06d3fd55cd1dcb405dd6b50dc2212ff2cd17de9c
Nicholas Tengbumroong <cherryappletruck@gmail.com>
2022-07-11 21:24:43 -0700

    Removed old post component layout

--- components/Post.js
+++ components/Post.js
@@ -6,30 +6,7 @@ function Post({ post }) {
     .split('\n')
     .map((str, index) => (str ? <p key={index}>{str}</p> : <br />));
 
-  const oldText = (
-    <article>
-      <a
-        rel="noopener noreferrer"
-        href="#"
-        className="grid p-4 overflow-hidden md:grid-cols-5 rounded-xl lg:p-6 xl:grid-cols-12 hover:bg-gray-50"
-      >
-        <h3 className="mb-1 ml-8 font-semibold md:col-start-2 md:col-span-4 md:ml-0 xl:col-start-3 xl:col-span-9">
-          {post.orgName}
-        </h3>
-        <time
-          dateTime=""
-          className="row-start-1 mb-1 md:col-start-1 xl:col-span-2 text-gray-600"
-        >
-          {post.date}
-        </time>
-        <div className="ml-8 md:col-start-2 md:col-span-4 xl:col-start-3 xl:col-span-9 md:ml-0 text-gray-700">
-          {text}
-        </div>
-      </a>
-    </article>
-  );
-
-  const newText = (
+  return (
     <div className="container flex flex-col w-full  p-6 mx-auto divide-y rounded-md divide-gray-300 bg-gray-50 text-gray-800">
       <div className="flex justify-between p-4">
         <div className="flex space-x-4">
@@ -52,8 +29,6 @@ function Post({ post }) {
       {post.msgAtch ? <Gallery images={post.msgAtch} /> : null}
     </div>
   );
-
-  return newText;
 }
 
 export default Post;
