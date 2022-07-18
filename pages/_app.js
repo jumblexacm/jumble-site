0b2705489822a0556870bae80e72b659e9f6a643
Nicholas Tengbumroong <61559177+nicholastengbumroong@users.noreply.github.com>
2022-07-18 15:54:09 -0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -14,7 +14,7 @@ function MyApp({ Component, pageProps }) {
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
-        gtag('config', '${process.env.GA_TRACKING_ID}', {
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
         page_path: window.location.pathname,
         });
       `}
