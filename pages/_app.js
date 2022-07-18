e9fba9f9a90aa43f41e4e04f17f0bc4cc6d7d002
Nicholas Tengbumroong <61559177+nicholastengbumroong@users.noreply.github.com>
2022-07-18 15:49:43 -0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ function MyApp({ Component, pageProps }) {
     <>
       <Script
         strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_TRACKING_ID}`}
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
       />
 
       <Script strategy="lazyOnload" id="google-analytics">
