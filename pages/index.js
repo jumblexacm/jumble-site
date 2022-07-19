51f8e5b7a91c2e748335498626c8ba2f830b5aa2
Nicholas Tengbumroong <cherryappletruck@gmail.com>
2022-07-18 18:34:46 -0700

    Updated to use MONGODB_DB env variable to specify database

--- pages/index.js
+++ pages/index.js
@@ -28,31 +28,30 @@ export default function Home({ isConnected, posts }) {
 
 export async function getServerSideProps(context) {
   try {
-    const client = await clientPromise //connects to Database when publishing
-    const db = client.db('JumbleDB')
-    const Posts = await db 
-    .collection("Posts")
-    .find({}, { projection: {message_id: 0 } })
-    .sort({_id: -1})
+    const client = await clientPromise; //connects to Database when publishing
+    const db = client.db(process.env.MONGODB_DB);
+    const Posts = await db
+      .collection('Posts')
+      .find({}, { projection: { message_id: 0 } })
+      .sort({ _id: -1 })
       // https://stackoverflow.com/a/5128574
       // _id, created by MongoDB and starts with document creation time
       // -1, signifies the sort is formatted descending (newest at top -> oldest)
-    //.limit(10)
-    .toArray()
+      //.limit(10)
+      .toArray();
 
     return {
       props: {
         isConnected: true,
         posts: JSON.parse(JSON.stringify(Posts)),
-      } 
-    } 
-  } 
-  catch (e) {
+      },
+    };
+  } catch (e) {
     console.error(e);
     return {
       props: {
-          isConnected: false,
+        isConnected: false,
       },
     };
-  } 
- };
+  }
+}
