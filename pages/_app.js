c7374f854c357b95ff526d8e83b018f11a626394
Nicholas Tengbumroong <cherryappletruck@gmail.com>
2022-07-19 13:57:56 -0700

    Added template navbar and implmented layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '../styles/globals.css';
 import Script from 'next/script';
+import Layout from '../components/Layout';
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -19,7 +20,9 @@ function MyApp({ Component, pageProps }) {
         });
       `}
       </Script>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </>
   );
 }
