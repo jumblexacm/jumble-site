29bdd3dd13067dfe033bd4840dc06a06e8903034
Nicholas Tengbumroong <cherryappletruck@gmail.com>
2022-07-20 12:27:43 -0700

    Limited posts on org profiles to 10

--- pages/organizations/[id].js
+++ pages/organizations/[id].js
@@ -2,7 +2,6 @@ import clientPromise from '../../lib/mongodb';
 import OrgProfile from '../../components/Organizations/OrgProfile';
 
 export default function OrgProfilePage({ posts }) {
-  console.log(posts);
   return (
     <div>
       <OrgProfile posts={posts} />
@@ -11,12 +10,12 @@ export default function OrgProfilePage({ posts }) {
 }
 
 export async function getServerSideProps({ params }) {
-  console.log(params.id);
   const db = (await clientPromise).db(process.env.MONGODB_DB);
   const posts = await db
     .collection('Posts')
     .find({ org_id: params.id }, { projection: { message_id: 0 } })
     .sort({ _id: -1 })
+    .limit(10)
     .toArray();
 
   return {
