270b87118be18a73658ffb3858ab1793f89aa297
Nicholas Tengbumroong <cherryappletruck@gmail.com>
2022-07-21 16:34:22 -0700

    Modified modal styling

--- components/Timeline/Modal.js
+++ components/Timeline/Modal.js
@@ -3,13 +3,13 @@ import Image from 'next/image';
 
 function Modal({ image, toggleModal }) {
   return (
-    <div className={styles.overlay}>
+    <div className={styles.overlay} onClick={toggleModal}>
       <div className={styles.modalContainer}>
-        <button className="absolute top-2 right-2" onClick={toggleModal}>
+        <button className="absolute top-1 right-1" onClick={toggleModal}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 512 512"
-            className="flex-shrink-0 w-6 h-6 fill-gray-500 hover:fill-black"
+            className="flex-shrink-0 w-6 h-6 fill-white hover:fill-gray-500"
           >
             <polygon points="427.314 107.313 404.686 84.687 256 233.373 107.314 84.687 84.686 107.313 233.373 256 84.686 404.687 107.314 427.313 256 278.627 404.686 427.313 427.314 404.687 278.627 256 427.314 107.313"></polygon>
           </svg>
