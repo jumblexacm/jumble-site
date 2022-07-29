258d1b0975fcf3741df95b1a477d69873ede03aa
Nicholas Tengbumroong <nicholastengbumroong@gmail.com>
2022-07-28 19:45:39 -0700

    Fixed footer repositioning

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -6,35 +6,37 @@ import styles from './Footer.module.css';
 function Footer() {
   return (
     <footer className={styles.footer}>
-      <div className={styles.logoWrapper}>
-        <Link href="/">
-          <a
-            rel="noopener noreferrer"
-            aria-label="Back to homepage"
-            className={styles.logoLink}
-          >
-            <div className={styles.imageWrapper}>
-              <Image
-                src={logo}
-                alt={'Jumble Logo'}
-                layout="fill"
-                objectFit="contain"
-              ></Image>
-            </div>
-          </a>
-        </Link>
+      <div className={styles.contentWrapper}>
+        <div className={styles.logoWrapper}>
+          <Link href="/">
+            <a
+              rel="noopener noreferrer"
+              aria-label="Back to homepage"
+              className={styles.logoLink}
+            >
+              <div className={styles.imageWrapper}>
+                <Image
+                  src={logo}
+                  alt={'Jumble Logo'}
+                  layout="fill"
+                  objectFit="contain"
+                ></Image>
+              </div>
+            </a>
+          </Link>
+        </div>
+        <ul className={styles.rightListWrapper}>
+          <li>
+            <p className={styles.rightListText}>Contact Us</p>
+            <a
+              href="mailto:admin@joinjumble.com"
+              className="hover:text-purple-600"
+            >
+              admin@joinjumble.com
+            </a>
+          </li>
+        </ul>
       </div>
-      <ul className={styles.rightListWrapper}>
-        <li>
-          <p className={styles.rightListText}>Contact Us</p>
-          <a
-            href="mailto:admin@joinjumble.com"
-            className="hover:text-purple-600"
-          >
-            admin@joinjumble.com
-          </a>
-        </li>
-      </ul>
     </footer>
   );
 }
