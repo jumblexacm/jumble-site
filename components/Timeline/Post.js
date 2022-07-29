e410a9dd19cf0055f55a32b276c9b38a3f2b4a1d
Nicholas Tengbumroong <cherryappletruck@gmail.com>
2022-07-28 17:18:33 -0700

    Created separate post css file and increased post font size

--- components/Timeline/Post.js
+++ components/Timeline/Post.js
@@ -10,44 +10,38 @@ function Post({ post }) {
 
   return (
     <div className={styles.postContainer}>
-      <div className="flex justify-between p-4">
-        <div className="flex space-x-4">
+      <div className={styles.postInfoWrapper}>
+        <Link
+          href={{
+            pathname: '/organizations/[id]',
+            query: { id: post.org_id },
+          }}
+        >
+          <a>
+            <div className={styles.authorImage}>
+              <Image
+                src={post.author_avatar_url}
+                alt={post.message_author}
+                layout="fill"
+                objectFit="cover"
+                className="rounded-full"
+              />
+            </div>
+          </a>
+        </Link>
+        <div>
           <Link
             href={{
               pathname: '/organizations/[id]',
               query: { id: post.org_id },
             }}
           >
-            <a>
-              <div className={styles.avatarImage}>
-                <Image
-                  src={post.author_avatar_url}
-                  alt={post.message_author}
-                  layout="fill"
-                  objectFit="cover"
-                  className="rounded-full"
-                />
-              </div>
-            </a>
+            <a className={styles.authorName}>{post.message_author}</a>
           </Link>
-          <div>
-            <Link
-              href={{
-                pathname: '/organizations/[id]',
-                query: { id: post.org_id },
-              }}
-            >
-              <a className="text-lg font-semibold hover:underline">
-                {post.message_author}
-              </a>
-            </Link>
-            <div className="mt-2 text-lg text-gray-600">{post.date}</div>
-          </div>
+          <div className={styles.postDate}>{post.date}</div>
         </div>
       </div>
-      <div className="p-4 space-y-2 text-sm text-gray-600 break-words">
-        {text}
-      </div>
+      <div className={styles.postText}>{text}</div>
       {post.attachment_urls.length != 0 ? (
         <Gallery images={post.attachment_urls} />
       ) : null}
