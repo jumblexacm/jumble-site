ad402c85eb394a05336ef2c6670a3f94ad862319
Nicholas Tengbumroong <nicholastengbumroong@gmail.com>
2022-07-28 19:14:27 -0700

    Fixed responsive footer resizing/repositioning

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -5,39 +5,36 @@ import styles from './Footer.module.css';
 
 function Footer() {
   return (
-    <footer className={styles.footerWrapper}>
-      <div className={styles.footer}>
-        <div className={styles.linkWrapper}>
-          <Link href="/">
-            <a
-              rel="noopener noreferrer"
-              aria-label="Back to homepage"
-              className={styles.link}
-            >
-              <div className={styles.imageWrapper}>
-                <Image
-                  src={logo}
-                  alt={'Jumble Logo'}
-                  layout="fill"
-                  objectFit="contain"
-                ></Image>
-              </div>
-            </a>
-          </Link>
-          <ul className={styles.leftListWrapper}>{/* Left side text */}</ul>
-        </div>
-        <ul className={styles.rightListWrapper}>
-          <li>
-            <p className="text-right">Contact Us</p>
-            <a
-              href="mailto:admin@joinjumble.com"
-              className="hover:text-purple-600"
-            >
-              admin@joinjumble.com
-            </a>
-          </li>
-        </ul>
+    <footer className={styles.footer}>
+      <div className={styles.logoWrapper}>
+        <Link href="/">
+          <a
+            rel="noopener noreferrer"
+            aria-label="Back to homepage"
+            className={styles.logoLink}
+          >
+            <div className={styles.imageWrapper}>
+              <Image
+                src={logo}
+                alt={'Jumble Logo'}
+                layout="fill"
+                objectFit="contain"
+              ></Image>
+            </div>
+          </a>
+        </Link>
       </div>
+      <ul className={styles.rightListWrapper}>
+        <li>
+          <p className={styles.rightListText}>Contact Us</p>
+          <a
+            href="mailto:admin@joinjumble.com"
+            className="hover:text-purple-600"
+          >
+            admin@joinjumble.com
+          </a>
+        </li>
+      </ul>
     </footer>
   );
 }
