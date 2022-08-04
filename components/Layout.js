362c30785b5c9ca9563841dd39b8f1e10e689262
Nicholas Tengbumroong <nicholastengbumroong@gmail.com>
2022-08-03 18:47:36 -0700

    Fixed minHeight for content in layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,12 @@
 import Navbar from './Navigation/Navbar';
 import Footer from './Footer/Footer';
+import { calculateObjectSize } from 'bson';
 
 function Layout({ children }) {
   return (
     <main>
       <Navbar />
-      {children}
+      <div style={{ minHeight: 'calc(100vh - 192px)' }}>{children}</div>
       <Footer />
     </main>
   );
