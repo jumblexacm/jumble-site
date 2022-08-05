d3b7e0391c02294950dea5f740143440662567c2
Nicholas Tengbumroong <nicholastengbumroong@gmail.com>
2022-08-04 20:58:47 -0700

    Added dynamic minHeight calc for inner div

--- components/Layout.js
+++ components/Layout.js
@@ -6,28 +6,33 @@ function Layout({ children }) {
   const navbarRef = useRef(null);
   const footerRef = useRef(null);
 
-  const [minHeight, setMinHeight] = useState();
+  const [dynamicHeight, setDynamicHeight] = useState('100vh');
 
+  // After initial render, will get height of header and footer to dynamically calculate minHeight of the inner div.
+  // Will force footer to always be at the bottom regardless of inner content.
   useEffect(() => {
-    setMinHeight(
-      navbarRef.current.clientHeight + footerRef.current.clientHeight
+    setDynamicHeight(
+      `calc(100vh - ${
+        navbarRef.current.clientHeight + footerRef.current.clientHeight
+      }px)`
     );
-  }, [minHeight]);
+    console.log(dynamicHeight);
+  }, [dynamicHeight]);
 
   return (
-    <main>
+    // This bg-color is a temporary fix to hide the gap between OrgList and Footer
+    <div className="bg-gray-200">
       <Navbar ref={navbarRef} />
       <div
-        // Makes sure the footer is always at the bottom of the screen
         style={{
-          minHeight: `calc(100vh - ${minHeight})`,
-          backgroundColor: '#e5e7eb',
+          minHeight: dynamicHeight,
+          backgroundColor: '#f9fafb', //// #f9fafb == bg-gray-50
         }}
       >
         {children}
       </div>
       <Footer ref={footerRef} />
-    </main>
+    </div>
   );
 }
 
