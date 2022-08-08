e1d79c37573e836fdc5ca02951e72a8650c8bb3c
Tech Visionaries <mrsiddhantgupta@gmail.com>
2022-08-08 14:48:49 -0700

    Created Our Story Page

---
+++ pages/ourstory/index.js
@@ -0,0 +1,8 @@
+import OurStory from '../../components/OurStory/OurStory.js';
+
+function OurStoryIndex() {
+        return (
+            <OurStory></OurStory>
+        )
+}
+export default OurStoryIndex;
