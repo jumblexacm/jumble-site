c219ee130ae5ca6fb95a52f3fdd63d50bfab6557
Nicholas Tengbumroong <nicholastengbumroong@gmail.com>
2022-08-08 12:18:22 -0700

    Added NoResults component for searches that return no hits

--- components/Search/SearchLanding.js
+++ components/Search/SearchLanding.js
@@ -4,13 +4,33 @@ import CustomPagination from './CustomPagination';
 import styles from './SearchLanding.module.css';
 import algoliasearch from 'algoliasearch/lite';
 import { InstantSearch, Configure, Hits } from 'react-instantsearch-hooks-web';
+import { NoResultsBoundary, NoResults } from './NoResults';
 
-const searchClient = algoliasearch(
+const INDEX_NAME = 'posts';
+
+const algoliaClient = algoliasearch(
   process.env.NEXT_PUBLIC_ALGOLIA_ID,
   process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_KEY
 );
 
-const INDEX_NAME = 'posts';
+const searchClient = {
+  ...algoliaClient,
+  search(requests) {
+    if (requests.every(({ params }) => !params.query)) {
+      return Promise.resolve({
+        results: requests.map(() => ({
+          hits: [],
+          nbHits: 0,
+          nbPages: 0,
+          page: 0,
+          processingTimeMS: 0,
+        })),
+      });
+    }
+
+    return algoliaClient.search(requests);
+  },
+};
 
 const Hit = ({ hit }) => {
   return (
@@ -25,13 +45,15 @@ function SearchLanding() {
   const query = router.query.query;
 
   return (
-    <div className='grow bg-gray-200'>
+    <div className={styles.landingWrapper}>
       <InstantSearch searchClient={searchClient} indexName={INDEX_NAME}>
         <Configure query={query} hitsPerPage={5} />
         <div className={styles.hitsContainer}>
-          <Hits hitComponent={Hit} />
+          <NoResultsBoundary fallback={<NoResults />}>
+            <Hits hitComponent={Hit} />
+            <CustomPagination padding={2} />
+          </NoResultsBoundary>
         </div>
-        <CustomPagination padding={2} />
       </InstantSearch>
     </div>
   );
