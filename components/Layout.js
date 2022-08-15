3fa6f7af88660fe7cd8ab207f5c4401a3e8330ee
Nicholas Tengbumroong <nicholastengbumroong@gmail.com>
2022-08-15 14:45:14 -0700

    Made navbar follow scroll

--- components/Layout.js
+++ components/Layout.js
@@ -7,6 +7,7 @@ function Layout({ children }) {
   const footerRef = useRef(null);
 
   const [dynamicHeight, setDynamicHeight] = useState('100vh');
+  const [navbarHeight, setNavbarHeight] = useState();
 
   // After initial render, will get height of header and footer to dynamically calculate minHeight of the inner div.
   // Will force footer to always be at the bottom regardless of inner content.
@@ -16,12 +17,14 @@ function Layout({ children }) {
         navbarRef.current.clientHeight + footerRef.current.clientHeight
       }px)`
     );
+    setNavbarHeight(navbarRef.current.clientHeight);
     console.log(dynamicHeight);
   }, [dynamicHeight]);
 
   return (
     <div>
       <Navbar ref={navbarRef} />
+      <div style={{ height: navbarHeight }} />
       <div
         className="flex flex-col"
         style={{
