d714f4f285539a964d1ec3529c557014f5526390
Kira Hammond <kirmar@stormyriver.com>
2022-08-28 22:16:51 -0700

    Tried to add user profile picture (too large)

--- components/Profile/UserProfile.js
+++ components/Profile/UserProfile.js
@@ -1,9 +1,22 @@
 import { useState } from 'react';
 import styles from './UserProfile.module.css';
+import Image from 'next/image';
+import Link from 'next/link';
 
 function UserProfile({ user }) {
   return (
-    <h4>Hello, {user?.name}!</h4>
+    <div className={styles.userProfileContainer}>
+      <div className={styles.userImageWrapper}>
+        <Image
+          src={user.picture}
+          alt=""
+          layout="fill"
+          objectFit="cover"
+          className="rounded-full"
+        />
+      </div>
+      <h4>Hello, {user?.name}!</h4>
+    </div>
   );
 }
 
