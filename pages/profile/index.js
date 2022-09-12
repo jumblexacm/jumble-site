0e46e68fc02f91023b08e514611df1e92fefc0d7
Kira Hammond <kirmar@stormyriver.com>
2022-09-11 23:02:04 -0700

    Simplified showing orgs
     * Switched from Axios back to fetch() because it works now for some reason
       and now having Axios available/working isn't necessary

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,5 +1,4 @@
 import clientPromise from '../../lib/mongodb';
-import axios from 'axios';
 import { useEffect } from 'react';
 import { useUser } from '@auth0/nextjs-auth0';
 import UserProfile from '../../components/Profile/UserProfile';
@@ -13,28 +12,22 @@ export default function UserProfilePage({ orgs }) {
   
   var orgIDs = {};
   if (!user) {
-    // console.log("no: ", user);
     orgIDs = {
       followed: [],
       managed: []
     };
-    console.log("no, orgIDs: ", orgIDs);
   } else {
-    // console.log("yes: ", user);
     orgIDs = {
       followed: user['https://ucrclubs.com/following'],
       managed: user['https://ucrclubs.com/adminFor']
     };
-    console.log("yes, orgIDs: ", orgIDs);
   }
-  
-  const fetchFollowedOrgs = async (orgIds) => {
-    axios.get(`api/user-orgs/${orgIds}`).then((res) => console.log(res));
-  };
   
   useEffect(() => {
     console.log(user);
-    fetchFollowedOrgs(JSON.stringify(orgIDs));
+    const orgIDsString = JSON.stringify(orgIDs);
+    fetch(`api/user-orgs/${orgIDsString}`)
+      .then((res) => console.log(res));
   });
   
   return (
@@ -42,33 +35,4 @@ export default function UserProfilePage({ orgs }) {
       <UserProfile user={user} orgs={orgs} />
     </div>
   );
-}
-
-export async function getServerSideProps({ params }) {
-  const db = (await clientPromise).db(process.env.MONGODB_DB);
-  
-  // const { user, error, isLoading } = useUser();
-  
-  var orgs = [];
-  
-  // TODO Remove hardcoded channel IDs
-  var org_ids = process.env.TMP_ORG_IDS.split(",");
-  // var org_ids = params.user.org_ids;
-  // var org_ids = user.org_ids;
-  
-  for (const org_id of org_ids) {
-    orgs.push(
-      await db
-        .collection('Orgs')
-        .find({ org_id: org_id })
-        .next()
-        // .sort({ recency: -1 })
-    );
-  }
-  
-  return {
-    props: {
-      orgs: JSON.parse(JSON.stringify(orgs)),
-    },
-  };
 }
