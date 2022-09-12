68e5c0c7df9a3d65f7d3bb04b97e1bcfb578dbef
Kira Hammond <kirmar@stormyriver.com>
2022-09-11 22:29:21 -0700

    Switched to Nick's Axios version of showing orgs
    Co-authored-by: Nicholas Tengbumroong <nicholastengbumroong@gmail.com>

--- pages/api/user-orgs/[user].js
+++ pages/api/user-orgs/[user].js
@@ -1,62 +1,65 @@
 // import clientPromise from '../../lib/mongodb';
 
 // export default function handler(req, res) {
-export async function handler(req, res) {
-  console.log("I'm here");
-  
-  const db = (await clientPromise).db(process.env.MONGODB_DB);
-  
-  // var mongoClient = require('mongodb').MongoClient;
-  // var db = mongoClient.connect(process.env.MONGODB_DB)
-  //   .then(function (db) {
-  //     console.log(db)
-  //   })
-  //   .catch(function (err) {});
-  
-  var followedOrgs = [];
-  var managedOrgs = [];
-  
+export default function handler(req, res) {
   console.log("req.query: ", req.query);
-  const orgIDs = JSON.parse(req.query.user);
-  console.log("orgIDs: ", orgIDs);
+  res.status(200).json({ name: 'John Doe' });
   
-  const followedOrgIDs = orgIDs['followed'];
-  const managedOrgIDs = orgIDs['managed'];
-  
-  console.log("1, ", followedOrgIDs);
-  console.log("2, ", managedOrgIDs);
-  
-  // // for (var orgID: followedOrgIDs) {
-  followedOrgIDs.forEach(function (orgID, index) {
-    followedOrgs.push(
-      await db
-        .collection('Orgs')
-        .find({ org_id: orgID })
-        .next()
-        // .sort({ recency: -1 })
-    );
-  });
-  // // }
+  // console.log("I'm here");
   // 
-  // // for (var orgID: managedOrgIDs) {
-  // managedOrgIDs.forEach(function (orgID, index) {
-  //   managedOrgs.push(
-  //     db.collection('Orgs')
+  // const db = (await clientPromise).db(process.env.MONGODB_DB);
+  // 
+  // // var mongoClient = require('mongodb').MongoClient;
+  // // var db = mongoClient.connect(process.env.MONGODB_DB)
+  // //   .then(function (db) {
+  // //     console.log(db)
+  // //   })
+  // //   .catch(function (err) {});
+  // 
+  // var followedOrgs = [];
+  // var managedOrgs = [];
+  // 
+  // console.log("req.query: ", req.query);
+  // const orgIDs = JSON.parse(req.query.user);
+  // console.log("orgIDs: ", orgIDs);
+  // 
+  // const followedOrgIDs = orgIDs['followed'];
+  // const managedOrgIDs = orgIDs['managed'];
+  // 
+  // console.log("1, ", followedOrgIDs);
+  // console.log("2, ", managedOrgIDs);
+  // 
+  // // // for (var orgID: followedOrgIDs) {
+  // followedOrgIDs.forEach(function (orgID, index) {
+  //   followedOrgs.push(
+  //     await db
+  //       .collection('Orgs')
   //       .find({ org_id: orgID })
   //       .next()
   //       // .sort({ recency: -1 })
   //   );
   // });
-  // // }
-  
-  console.log("followedOrgs: ", followedOrgs);
-  
-  res.status(200).json({
-    followedOrgs: JSON.parse(JSON.stringify(followedOrgs)),
-    managedOrgs: JSON.parse(JSON.stringify(managedOrgs))
-  });
-  
-  // // TODO Remove hardcoded channel IDs
-  // // var org_ids = process.env.TMP_ORG_IDS.split(",");
-  // // var org_ids = params.user.org_ids;
+  // // // }
+  // // 
+  // // // for (var orgID: managedOrgIDs) {
+  // // managedOrgIDs.forEach(function (orgID, index) {
+  // //   managedOrgs.push(
+  // //     db.collection('Orgs')
+  // //       .find({ org_id: orgID })
+  // //       .next()
+  // //       // .sort({ recency: -1 })
+  // //   );
+  // // });
+  // // // }
+  // 
+  // console.log("followedOrgs: ", followedOrgs);
+  // 
+  // res.status(200).json({
+  //   followedOrgs: JSON.parse(JSON.stringify(followedOrgs)),
+  //   managedOrgs: JSON.parse(JSON.stringify(managedOrgs))
+  // });
+  // 
+  // // // TODO Remove hardcoded channel IDs
+  // // // var org_ids = process.env.TMP_ORG_IDS.split(",");
+  // // // var org_ids = params.user.org_ids;
 }
--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,16 +1,15 @@
-// import clientPromise from '../../lib/mongodb';
-import { useEffect, useState } from 'react';
+import clientPromise from '../../lib/mongodb';
+import axios from 'axios';
+import { useEffect } from 'react';
 import { useUser } from '@auth0/nextjs-auth0';
 import UserProfile from '../../components/Profile/UserProfile';
 
 // Source: https://auth0.github.io/nextjs-auth0/modules/frontend_use_user.html
-export default function UserProfilePage({ user_email, orgs }) {
-  const { user, error, tmpLoading } = useUser();
+export default function UserProfilePage({ orgs }) {
+  const { user, error, isLoading } = useUser();
   
-  if (tmpLoading) return <div>Loading...</div>;
+  if (isLoading) return <div>Loading...</div>;
   if (error) return <div>{error.message}</div>;
-  
-  console.log(user);
   
   var orgIDs = {};
   if (!user) {
@@ -29,53 +28,14 @@ export default function UserProfilePage({ user_email, orgs }) {
     console.log("yes, orgIDs: ", orgIDs);
   }
   
-  console.log(`/api/user-orgs/` + JSON.stringify(orgIDs));
+  const fetchFollowedOrgs = async (orgIds) => {
+    axios.get(`api/user-orgs/${orgIds}`).then((res) => console.log(res));
+  };
   
-  // var data;
-  // var tmp;
-  
-  // https://nextjs.org/docs/basic-features/data-fetching/client-side#client-side-data-fetching-with-useeffect
-  const [data, setData] = useState(null);
-  const [isLoading, setLoading] = useState(false);
   useEffect(() => {
-    // setLoading(true)
-    // fetch(`/api/user-orgs/${user}`)
-    fetch(`/api/user-orgs/` + JSON.stringify(orgIDs))
-      // .then((res) => {data = res.json(); tmp = res; console.log(data);})
-      // .then((res) => {
-      //   console.log("res: ", res);
-      //   var followedOrgs = res['followedOrgs'];
-      //   var managedOrgs = res['managedOrgs'];
-      // })
-      .then((res) => res.json())
-      .then((data) => {
-        setData(data)
-        setLoading(false)
-      })
-  }, [])
-  
-  console.log("data: ", data);
-  // console.log("tmp: ", tmp);
-  // var followedOrgs = data['followedOrgs'];
-  // var managedOrgs = data['managedOrgs'];
-  
-  // var orgs = res.followedOrgs;
-  
-  // if (!user_email) {
-  //   return (
-  //     <div>
-  //       No user ID
-  //     </div>
-  //   )
-  // }
-  // 
-  // if (user && user_email && user.email != user_email) {
-  //   return (
-  //     <div>
-  //       Hello, world!
-  //     </div>
-  //   )
-  // }
+    console.log(user);
+    fetchFollowedOrgs(user['https://ucrclubs.com/following'].toString());
+  });
   
   return (
     <div className="flex flex-col grow">
@@ -84,37 +44,31 @@ export default function UserProfilePage({ user_email, orgs }) {
   );
 }
 
-// export async function getServerSideProps({ params }) {
-//   const db = (await clientPromise).db(process.env.MONGODB_DB);
-// 
-//   if (params.user_email) {
-//     var user = await db
-//       .collection('Users')
-//       .find({ user_email: params.user_email })
-//       .next();
-// 
-//     var orgs = [];
-// 
-//     // TODO Remove hardcoded channel IDs
-//     // var org_ids = process.env.TMP_ORG_IDS.split(",");
-//     // var org_ids = params.user.org_ids;
-//     var org_ids = user.org_ids;
-// 
-//     for (const org_id of org_ids) {
-//       orgs.push(
-//         await db
-//           .collection('Orgs')
-//           .find({ org_id: org_id })
-//           .next()
-//           // .sort({ recency: -1 })
-//       );
-//     }
-//   }
-// 
-//   return {
-//     props: {
-//       user_email: params.user_email,
-//       orgs: JSON.parse(JSON.stringify(orgs)),
-//     },
-//   };
-// }
+export async function getServerSideProps({ params }) {
+  const db = (await clientPromise).db(process.env.MONGODB_DB);
+  
+  // const { user, error, isLoading } = useUser();
+  
+  var orgs = [];
+  
+  // TODO Remove hardcoded channel IDs
+  var org_ids = process.env.TMP_ORG_IDS.split(",");
+  // var org_ids = params.user.org_ids;
+  // var org_ids = user.org_ids;
+  
+  for (const org_id of org_ids) {
+    orgs.push(
+      await db
+        .collection('Orgs')
+        .find({ org_id: org_id })
+        .next()
+        // .sort({ recency: -1 })
+    );
+  }
+  
+  return {
+    props: {
+      orgs: JSON.parse(JSON.stringify(orgs)),
+    },
+  };
+}
