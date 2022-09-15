e527f60ca6fb67dcb35783028fdd80d92c3db7bf
Kira Hammond <kirmar@stormyriver.com>
2022-09-15 15:53:54 -0700

    Add Cooper Illustrations to Our Story page
     * From Cannon's link: https://www.figma.com/community/file/994232780261959616

--- components/OurStory/OurStory.js
+++ components/OurStory/OurStory.js
@@ -1,4 +1,9 @@
 import styles from './OurStory.module.css';
+import Image from 'next/image';
+import dancing from '../../public/cooper-dancing.png';
+import statistics from '../../public/cooper-statistics.png';
+import gaming from '../../public/cooper-gaming.png';
+
 function OurStory() {
   return (
     <div className={styles.ourStoryWrapper}>
@@ -11,15 +16,42 @@ function OurStory() {
             `UCR Clubs was created to provide a simpler process for students at the University of California, Riverside to discover clubs & organizations on campus.`
           }
         </div>
+        <div className={styles.imageWrapper}>
+          <Image
+            src={dancing}
+            alt="Two cartoon characters dancing with musical notes around them"
+            layout="fill"
+            objectFit="cover"
+            className="rounded-full"
+          />
+        </div>
         <div className={styles.ourStoryParagraph}>
           {
             `As UCR students, we know that looking through countless pages to find your niche can be time-consuming and disheartening. So, we’ve designed and built UCR Clubs to facilitate rapid discovery by presenting the most up-to-date information on clubs at UCR.`
           }
         </div>
+        <div className={styles.imageWrapper}>
+          <Image
+            src={statistics}
+            alt="Two carton characters pointing at a board with a pie chart and bar chart"
+            layout="fill"
+            objectFit="cover"
+            className="rounded-full"
+          />
+        </div>
         <div className={styles.ourStoryParagraph}>
           {
             `You’re in good company. UCR’s diverse community is the foundation of our rich variety of organizations where you’re bound to discover clubs that match your many hobbies and passions.`
           }
+        </div>
+        <div className={styles.imageWrapper}>
+          <Image
+            src={gaming}
+            alt="Two cartoon characters sitting on couches playing a video game on a TV"
+            layout="fill"
+            objectFit="cover"
+            className="rounded-full"
+          />
         </div>
         <div className={styles.ourStoryParagraph}>
           {
